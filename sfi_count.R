cb9f70ef183e76bc2cd7a3482c3c38d244176f95
hopeful-coder <mschepps21@gmail.com>
2019-10-19 08:42:47 -0700

    Intermediary deletions and commentary put in

--- sfi_count.R
+++ sfi_count.R
@@ -1,52 +1,43 @@
 library(dplyr)
 library(plyr)
-#Old data
-# pop <- read.csv('C:/Users/msche/OneDrive/Desktop/UCLAQuarterlyReportPopulation.csv', skip = 1)
-# 
-# arrests <- read.csv('C:/Users/msche/OneDrive/Desktop/UCLAQuarterlyReportArrests.csv')
-# 
 
 setwd('C:/Users/Mitchell Schepps/Desktop/nena')
 #Load in new data
 pop <- read.csv('UCLAQuarterlyReportSeptember2019Population.csv', skip = 1)
-
 arrests <- read.csv('UCLAQuarterlyReportSeptember2019Arrests.csv')
-pfs2 <- read.csv('payforsuccess_flatfile_june_190819.csv')
+
 #Formal quarter dates
 dates <- as.Date(c('10-23-2017', '01-01-2018', '04-01-2018', '07-01-2018', '10-01-2018',
                    '01-01-2019', '04-01-2019', '07-01-2019', '10-01-2019',
                    '01-01-2020', '04-01-2020', '07-01-2020', '10-01-2020'), format = '%m-%d-%Y')
-
+#Remove initial date for calculations
 dates <- dates[-1]
-#Rename columns to match
-names(pop)[1] <- 'id'
-names(arrests)[1] <- 'id'
 
 ###############################################################################
 #Incorporate new information.
 ###############################################################################
+#Rename columns to match
+names(pop)[1] <- 'id'
+names(arrests)[1] <- 'id'
+
+#Load in NCC Data
 ncc1 <- read.csv('NCC1_june.csv', skip = 12, header = T)
 ncc2 <- read.csv('NCC2_june.csv', skip = 12, header = T)
-
+#Clean up NCC Data for merging purposes
 ncc1 <- ncc1[,-1]
 ncc2 <- ncc2[,-1]
 ncc1 <- ncc1[1:4,]
-
 names(ncc1)[1] <- 'id'
 names(ncc2)[1] <- 'id'
 names(ncc1)[5] <- 'ncc_date'
 names(ncc2)[5] <- 'ncc_date'
-
 ncc <- rbind(ncc1[,c(1,5)], ncc2[,c(1,5)])
 
-
-#Merge population dataset and arrest dataset
+#Merge population dataset and arrest dataset and ncc dataset
 test <- merge(pop, arrests, by = 'id', all.x = T)
 test <- merge(test, ncc, by = 'id', all.x = T)
 test$ncc_date <- as.Date(test$ncc_date, format = '%m/%d/%Y')
 
-
-#Subtract dates to get relevant quarter information.
 #Format dates properly
 test2 <- test
 test2$X.5 <- as.Date(test2$X.5, format = '%m/%d/%Y')
@@ -55,20 +46,20 @@ test2$Intake.Date <- as.Date(test2$Intake.Date, format = '%m/%d/%Y')
 test2$Referral.Date <- as.Date(test2$Referral.Date, format = '%m/%d/%Y')
 test2$Arrest.Date <- as.Date(test2$Arrest.Date, format = '%m/%d/%Y')
 
+#Potential ids to watch out for.
 # test2 <- test2[!is.na(test2$Intake.Date), ]
 # test2 <- test2[!test2$id == 3239845,]
 # test2 <- test2[!test2$id == 4575647,]
+
+#List of unique ids
 unique.ids <- unique(test2$id)
 
 #Make a loop which figures out which quarter in the formal dates the clean quarter if any happened in
-final <- c()
-overall <- c()
 abbie <- c()
 abbie <- data.frame(abbie)
 abbie.final <- c()
 abbie.final <- data.frame(abbie.final)
 for(i in 1:length(unique.ids)){
-  zz = i
   #Extract one person's data to analyze at a time.
   words <- test2[test2$id == unique.ids[i], ]
   
@@ -151,10 +142,6 @@ for(i in 1:length(unique.ids)){
   abbie$cq5 <- ifelse(5 %in% quarters_achieved, sum(quarters_achieved == 5), 0)
   abbie$cq6 <- ifelse(6 %in% quarters_achieved, sum(quarters_achieved == 6), 0)
   abbie$cq7 <- ifelse(7 %in% quarters_achieved, sum(quarters_achieved == 7), 0)
-  
-  
-  testing <- c(cq.dates, row)
-  sorted.all <- sort(testing)
   
   #Strict arrest days array for making abbie
   arrests.abbie <- c(words$Arrest.Date)
@@ -328,15 +315,7 @@ for(i in 1:length(unique.ids)){
                              ifelse(abbie$cq6 != 0 & abbie$pq6 == 0, 0, NA))
   abbie$july_sep19 <- ifelse(abbie$cq7 != 0 & abbie$pq7 != 0, abbie$pq7,
                              ifelse(abbie$cq7 != 0 & abbie$pq7 == 0, 0, NA))
-  # abbie$jan_march18 <- ifelse(abbie$cq1 == 1 & abbie$pq1 == 1, 1,
-  #                    ifelse(abbie$cq1 == 1 & abbie$pq1 == 0, 0, NA))
-  # abbie$apr_june18 <- ifelse(abbie$cq2 == 1 & abbie$pq2 == 1, 1,
-  #                ifelse(abbie$cq2 == 1 & abbie$pq2 == 0, 0, NA))
-  # abbie$july_sep18 <- ifelse(abbie$cq3 == 1 & abbie$pq3 == 1, 1,
-  #                    ifelse(abbie$cq3 == 1 & abbie$pq3 == 0, 0, NA))
-  # abbie$oct_dec18 <- ifelse(abbie$cq4 == 1 & abbie$pq4 == 1, 1,
-  #                    ifelse(abbie$cq4 == 1 & abbie$pq4 == 0, 0, NA))
-  # 
+
   ncc_date = unique(words$ncc_date)
   abbie$ncc = ncc_date
   abbie.final <- rbind.fill(abbie.final, abbie)
@@ -355,11 +334,36 @@ pfs <- pfs[!(pfs$Intake.Date == ' '), ]
 
 write.csv(pfs, 'Payforsuccess_flatfile_september19_191018.csv', row.names = F)
 
+
+###############################################################################
+#Start of testing to be done
+###############################################################################
 #Formal z-test
 num.arrested.pfs <- sum(pfs$ever.arrested)
 num.arrested.pas <- sum(pas$ever.arrested)
 
 prop.test(c(num.arrested.pfs, num.arrested.pas), n = c(nrow(pfs), nrow(pas)))
+
+
+###############################################################################
+#Diagnositics procedures
+#Probably old and can be updated.
+###############################################################################
+abbie.final = abbie.final[abbie.final$id %in% pfs$id, ]
+
+abbie.final$cq1 == pfs$cq1
+abbie.final$cq2 == pfs$cq2
+abbie.final$cq3 == pfs$cq3
+abbie.final$cq4 == pfs$cq4
+abbie.final$cq5 == pfs$cq5
+
+abbie.final$pq1 == pfs$pq1
+abbie.final$pq2 == pfs$pq2
+abbie.final$pq3 == pfs$pq3
+abbie.final$pq4 == pfs$pq4
+abbie.final$pq5 == pfs$pq5
+
+pfs2 <- read.csv('payforsuccess_flatfile_june_190819.csv')
 
 pfs.arrests <- pfs[!is.na(pfs$arrest1),]
 pas.arrests <- pas[!is.na(pas$arrest1),]
