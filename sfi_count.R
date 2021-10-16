a3185697273c6ce408fdf072ef1bea56720f26f2
mschepps21@gmail.com <mschepps21@gmail.com>
2021-10-16 04:37:40 -0700

    sep21

--- sfi_count.R
+++ sfi_count.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(plyr)
 
-setwd('C:/Users/Mitchell Schepps/Desktop/nena/arrests_pop')
+setwd("C:/Users/Admin/Desktop/Github/SFI")
 # #Load in new data
 pop <- read.csv('UCLAQuarterlyReportJune2020Population.csv', skip = 1)
 arrests <- read.csv('UCLAQuarterlyReportJune2020Arrests.csv')
@@ -9,10 +9,19 @@ arrests <- read.csv('UCLAQuarterlyReportJune2020Arrests.csv')
 # pop <- read.csv('UCLAQuarterlyReportDecember2019Population.csv', skip = 1)
 # arrests <- read.csv('UCLAQuarterlyReportDecember2019Arrests.csv')
 
+pop <- read.csv("dec2020pop.csv", skip = 1)
+arrests <- read.csv("dec2020arrests.csv")
+#2873957 = 21 arrests
+pop <- read.csv("sep2021pop.csv", skip = 1)
+arrests <- read.csv("sep2021arrests.csv")
+# pop <- read.csv("sep2020pop.csv", skip = 1)
+# arrests <- read.csv("sep2020arrests.csv")
 #Formal quarter dates
 dates <- as.Date(c('10-23-2017', '01-01-2018', '04-01-2018', '07-01-2018', '10-01-2018',
                    '01-01-2019', '04-01-2019', '07-01-2019', '10-01-2019',
-                   '01-01-2020', '04-01-2020', '07-01-2020', '10-01-2020'), format = '%m-%d-%Y')
+                   '01-01-2020', '04-01-2020', '07-01-2020', '10-01-2020',
+                   '01-01-2021', '04-01-2021', '07-01-2021', '10-01-2021',
+                   '01-01-2022'), format = '%m-%d-%Y')
 #Remove initial date for calculations
 dates <- dates[-1]
 
@@ -40,9 +49,11 @@ names(arrests)[1] <- 'id'
 # ncc[ncc$id == '4852861/4589517', 'id'] = '4852861'
 # dup = which(duplicated(ncc$id))
 # ncc = ncc[-dup, ]
-setwd('C:/Users/Mitchell Schepps/Desktop/nena')
 #New ncc
-ncc = read.csv('ncc_630.csv', skip = 9)
+# ncc <- read.csv("dec2020ncc.csv", header=TRUE, skip = 9)
+ncc <- read.csv("sep2021ncc.csv", header=TRUE, skip = 9)
+
+# ncc <- read.csv("sep2020ncc.csv", header=TRUE, skip = 9)
 
 names(ncc)[1] = 'id'
 names(ncc)[9] = 'ncc_date'
@@ -62,7 +73,6 @@ test2$Referral.Date <- as.Date(test2$Referral.Date, format = '%m/%d/%Y')
 test2$Arrest.Date <- as.Date(test2$Arrest.Date, format = '%m/%d/%Y')
 
 #Sort out cohorts
-ash = test2
 # test2 = test2[test2$X.5 < as.Date('06/01/2019', format = '%m/%d/%Y'),]
 #Potential ids to watch out for.
 # test2 <- test2[!is.na(test2$Intake.Date), ]
@@ -143,7 +153,22 @@ for(i in 1:length(unique.ids)){
     placement = which(cq.dates == x)
     cq.dates[placement] = x + 1
   }
+  x = as.Date('2021-04-01', format = '%Y-%m-%d')
+  if(any(cq.dates == x)){
+    placement = which(cq.dates == x)
+    cq.dates[placement] = x + 1
+  }
   x = as.Date('2020-07-01', format = '%Y-%m-%d')
+  if(any(cq.dates == x)){
+    placement = which(cq.dates == x)
+    cq.dates[placement] = x + 1
+  }
+  x = as.Date('2021-01-01', format = '%Y-%m-%d')
+  if(any(cq.dates == x)){
+    placement = which(cq.dates == x)
+    cq.dates[placement] = x + 1
+  }
+  x = as.Date('2021-07-01', format = '%Y-%m-%d')
   if(any(cq.dates == x)){
     placement = which(cq.dates == x)
     cq.dates[placement] = x + 1
@@ -183,6 +208,11 @@ for(i in 1:length(unique.ids)){
   abbie$cq8 <- ifelse(8 %in% quarters_achieved, sum(quarters_achieved == 8), 0)
   abbie$cq9 <- ifelse(9 %in% quarters_achieved, sum(quarters_achieved == 9), 0)
   abbie$cq10 <- ifelse(10 %in% quarters_achieved, sum(quarters_achieved == 10), 0)
+  abbie$cq11 <- ifelse(11 %in% quarters_achieved, sum(quarters_achieved == 11), 0)
+  abbie$cq12 <- ifelse(12 %in% quarters_achieved, sum(quarters_achieved == 12), 0)
+  abbie$cq13 <- ifelse(13 %in% quarters_achieved, sum(quarters_achieved == 13), 0)
+  abbie$cq14 <- ifelse(14 %in% quarters_achieved, sum(quarters_achieved == 14), 0)
+  abbie$cq15 <- ifelse(15 %in% quarters_achieved, sum(quarters_achieved == 15), 0)
   
   #Strict arrest days array for making abbie
   abbie$num.arrests = 0
@@ -192,39 +222,24 @@ for(i in 1:length(unique.ids)){
     abbie$num.arrests = length(arrests.abbie)
     if(length(arrests.abbie) == 1){
       abbie$arrest1 <- arrests.abbie
-      abbie$arrest1type <- arrests.type
     } else if(length(arrests.abbie) == 2){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
     } else if(length(arrests.abbie) == 3){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
       abbie$arrest3 <- arrests.abbie[3]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
     } else if(length(arrests.abbie) == 4){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
       abbie$arrest3 <- arrests.abbie[3]
       abbie$arrest4 <- arrests.abbie[4]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
     } else if(length(arrests.abbie) == 5){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
       abbie$arrest3 <- arrests.abbie[3]
       abbie$arrest4 <- arrests.abbie[4]
       abbie$arrest5 <- arrests.abbie[5]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest4type <- arrests.type[5]
     } else if(length(arrests.abbie) == 6){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -232,12 +247,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest4 <- arrests.abbie[4]
       abbie$arrest5 <- arrests.abbie[5]
       abbie$arrest6 <- arrests.abbie[6]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
     } else if(length(arrests.abbie) == 7){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -246,13 +255,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest5 <- arrests.abbie[5]
       abbie$arrest6 <- arrests.abbie[6]
       abbie$arrest7 <- arrests.abbie[7]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
     } else if(length(arrests.abbie) == 8){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -262,14 +264,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest6 <- arrests.abbie[6]
       abbie$arrest7 <- arrests.abbie[7]
       abbie$arrest8 <- arrests.abbie[8]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
     } else if(length(arrests.abbie) == 9){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -280,15 +274,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest7 <- arrests.abbie[7]
       abbie$arrest8 <- arrests.abbie[8]
       abbie$arrest9 <- arrests.abbie[9]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
-      abbie$arrest9type <- arrests.type[9]
     } else if(length(arrests.abbie) == 10){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -300,16 +285,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest8 <- arrests.abbie[8]
       abbie$arrest9 <- arrests.abbie[9]
       abbie$arrest10 <- arrests.abbie[10]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
-      abbie$arrest9type <- arrests.type[9]
-      abbie$arrest10type <- arrests.type[10]
     } else if(length(arrests.abbie) == 11){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -322,17 +297,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest9 <- arrests.abbie[9]
       abbie$arrest10 <- arrests.abbie[10]
       abbie$arrest11 <- arrests.abbie[11]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
-      abbie$arrest9type <- arrests.type[9]
-      abbie$arrest10type <- arrests.type[10]
-      abbie$arrest11type <- arrests.type[11]
     } else if(length(arrests.abbie) == 12){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -346,18 +310,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest10 <- arrests.abbie[10]
       abbie$arrest11 <- arrests.abbie[11]
       abbie$arrest12 <- arrests.abbie[12]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
-      abbie$arrest9type <- arrests.type[9]
-      abbie$arrest10type <- arrests.type[10]
-      abbie$arrest11type <- arrests.type[11]
-      abbie$arrest12type <- arrests.type[12]
     }else if(length(arrests.abbie) == 13){
       abbie$arrest1 <- arrests.abbie[1]
       abbie$arrest2 <- arrests.abbie[2]
@@ -372,19 +324,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest11 <- arrests.abbie[11]
       abbie$arrest12 <- arrests.abbie[12]
       abbie$arrest13 <- arrests.abbie[13]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
-      abbie$arrest9type <- arrests.type[9]
-      abbie$arrest10type <- arrests.type[10]
-      abbie$arrest11type <- arrests.type[11]
-      abbie$arrest12type <- arrests.type[12]
-      abbie$arrest13type <- arrests.type[13]
     }
     else if(length(arrests.abbie) == 14){
       abbie$arrest1 <- arrests.abbie[1]
@@ -401,20 +340,6 @@ for(i in 1:length(unique.ids)){
       abbie$arrest12 <- arrests.abbie[12]
       abbie$arrest13 <- arrests.abbie[13]
       abbie$arrest14 <- arrests.abbie[14]
-      abbie$arrest1type <- arrests.type[1]
-      abbie$arrest2type <- arrests.type[2]
-      abbie$arrest3type <- arrests.type[3]
-      abbie$arrest4type <- arrests.type[4]
-      abbie$arrest5type <- arrests.type[5]
-      abbie$arrest6type <- arrests.type[6]
-      abbie$arrest7type <- arrests.type[7]
-      abbie$arrest8type <- arrests.type[8]
-      abbie$arrest9type <- arrests.type[9]
-      abbie$arrest10type <- arrests.type[10]
-      abbie$arrest11type <- arrests.type[11]
-      abbie$arrest12type <- arrests.type[12]
-      abbie$arrest13type <- arrests.type[13]
-      abbie$arrest14type <- arrests.type[14]
     }
   }
   
@@ -449,6 +374,7 @@ for(i in 1:length(unique.ids)){
   arrest.inter$eig = sum(arrest.quarter.list == 8)
   arrest.inter$nin = sum(arrest.quarter.list == 9)
   arrest.inter$ten = sum(arrest.quarter.list == 10)
+  arrest.inter$ele = sum(arrest.quarter.list == 11)
   
   arrest.matrix = rbind(arrest.matrix, arrest.inter)
   #Not sure if this will fix the issue where 2 potential quarters, but both were arrested in.
@@ -466,6 +392,11 @@ for(i in 1:length(unique.ids)){
   abbie$pq8 <- ifelse(8 %in% quarters_achieved, sum(quarters_achieved == 8), 0)
   abbie$pq9 <- ifelse(9 %in% quarters_achieved, sum(quarters_achieved == 9), 0)
   abbie$pq10 <- ifelse(10 %in% quarters_achieved, sum(quarters_achieved == 10), 0)
+  abbie$pq11 <- ifelse(11 %in% quarters_achieved, sum(quarters_achieved == 11), 0)
+  abbie$pq12 <- ifelse(12 %in% quarters_achieved, sum(quarters_achieved == 12), 0)
+  abbie$pq13 <- ifelse(13 %in% quarters_achieved, sum(quarters_achieved == 13), 0)
+  abbie$pq14 <- ifelse(14 %in% quarters_achieved, sum(quarters_achieved == 14), 0)
+  abbie$pq15 <- ifelse(15 %in% quarters_achieved, sum(quarters_achieved == 15), 0)
   
   
   abbie[abbie$id == 3334613, 'cq5'] = 0
@@ -487,11 +418,20 @@ for(i in 1:length(unique.ids)){
                              ifelse(abbie$cq7 != 0 & abbie$pq7 == 0, 0, NA))
   abbie$oct_dec19 <- ifelse(abbie$cq8 != 0 & abbie$pq8 != 0, abbie$pq8,
                             ifelse(abbie$cq8 != 0 & abbie$pq8 == 0, 0, NA))
-  abbie$jan_mar20 <- ifelse(abbie$cq9 != 0 & abbie$pq9 != 0, abbie$pq9,
+  abbie$jan_march20 <- ifelse(abbie$cq9 != 0 & abbie$pq9 != 0, abbie$pq9,
                             ifelse(abbie$cq9 != 0 & abbie$pq9 == 0, 0, NA))
   abbie$apr_june20 <- ifelse(abbie$cq10 != 0 & abbie$pq10 != 0, abbie$pq10,
                             ifelse(abbie$cq10 != 0 & abbie$pq10 == 0, 0, NA))
-
+  abbie$july_sep20 <- ifelse(abbie$cq11 != 0 & abbie$pq11 != 0, abbie$pq11,
+                             ifelse(abbie$cq11 != 0 & abbie$pq11 == 0, 0, NA))
+  abbie$oct_dec20 <- ifelse(abbie$cq12 != 0 & abbie$pq12 != 0, abbie$pq12,
+                             ifelse(abbie$cq12 != 0 & abbie$pq12 == 0, 0, NA))  
+  abbie$jan_march21 <- ifelse(abbie$cq13 != 0 & abbie$pq13 != 0, abbie$pq13,
+                            ifelse(abbie$cq13 != 0 & abbie$pq13 == 0, 0, NA))
+  abbie$apr_june21 <- ifelse(abbie$cq14 != 0 & abbie$pq14 != 0, abbie$pq14,
+                              ifelse(abbie$cq14 != 0 & abbie$pq14 == 0, 0, NA)) 
+  abbie$july_sep21 <- ifelse(abbie$cq15 != 0 & abbie$pq15 != 0, abbie$pq15,
+                              ifelse(abbie$cq15 != 0 & abbie$pq15 == 0, 0, NA)) 
   ncc_date = unique(words$ncc_date)
   abbie$ncc = ncc_date
   abbie.final <- rbind.fill(abbie.final, abbie)
@@ -502,24 +442,26 @@ abbie.final2 <- merge(abbie.final, pop[, c('id', 'X.6', 'Intake.Date')], by = 'i
 indice = which(names(abbie.final2) == 'X.6')
 names(abbie.final2)[indice] = 'RandomGroup'
 # write.csv(abbie.final, 'ucla_arrest_data_190127.csv', row.names = F)
+abbie.final2$cohort2 = ifelse(abbie.final2$randomization.date <= as.Date('05-31-2019', format = '%m-%d-%Y'), 1, 0)
+
 
 
 pfs <- abbie.final2[abbie.final2$RandomGroup == 'Pay for Success',]               
 pas <- abbie.final2[abbie.final2$RandomGroup == 'Probation as Usual', ]               
 
 pfs <- pfs[!(pfs$Intake.Date == ' '), ]
-pfs <- (pfs[as.Date(pfs$Intake.Date, format = '%m/%d/%Y') <= as.Date('06/30/2020', format = '%m/%d/%Y'),])
+pfs <- (pfs[as.Date(pfs$Intake.Date, format = '%m/%d/%Y') <= as.Date('09/30/2021', format = '%m/%d/%Y'),])
 
 # pfs$arrest9 = NULL
 
-pfs = pfs[,-c(56,57,58,59,63,64,65,66,68,70)]
+# pfs = pfs[,-c(35, 61,62,63,68,69,70,71,72,74,77,78)]
 
-write.csv(pfs, 'Payforsuccess_flatfile_June20.csv', row.names = F)
+write.csv(pfs, 'Payforsuccess_flatfile_September21.csv', row.names = F)
 
 #Create dataframe for final numbers needed.
 final <- data.frame(clean = c(),
                     potential = c())
-for(i in 1:10){
+for(i in 1:15){
   word.used.p = paste0('pq', i)
   word.used.c = paste0('cq', i)
   
